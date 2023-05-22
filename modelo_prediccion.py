0244117993449b308a56262ddac94e39d52cd692
Vir <Vir>
2023-05-22 13:11:44 -0300

    No va

--- modelo_prediccion.py
+++ modelo_prediccion.py
@@ -15,13 +15,14 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
+from modelo_entrenamiento import random_forest 
 
-!pip install; random_forest
+# !pip install; random_forest
 
 """#OBTENCIÓN DEL DATA SET"""
 
 #Lee el csv y lo convierte a un df de pandas
-path = "C:\\Users\\Vir\\Desktop\\SDIA\\2-CIENTIFICO DE DATOS\\PP2\\PP2_MODELO_DT\\PP2_MODELO\\test.csv"
+path = "test.csv"
 dataset_crudo = pd.read_csv(path)
 dataset_crudo
 
@@ -30,7 +31,8 @@ dataset_crudo
 #ruta_archivo = os.path.abspath("modelo_entrenamiento.py")
 #print(ruta_archivo)
 
-from "C:\\Users\\Vir\\Desktop\\SDIA\\2-CIENTIFICO DE DATOS\\PP2\\PP2_MODELO_DT\\PP2_MODELO" ; import random_forest
+# from "C:\\Users\\Vir\\Desktop\\SDIA\\2-CIENTIFICO DE DATOS\\PP2\\PP2_MODELO_DT\\PP2_MODELO" ; import random_forest 
+
 
 #Predecimos con los datos del TEST
 random_forest_prediction = random_forest.predict(Datos_test_normalizado)
