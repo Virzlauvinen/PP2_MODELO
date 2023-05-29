e7b767192c79762e914aa73c31c36dec372bc5cd
Vir <Vir>
2023-05-29 13:28:59 -0300

    Prueba estatica en archivo prediccion.py

--- flaskapp/src/modelo_prediccion.py
+++ flaskapp/src/modelo_prediccion.py
@@ -2,42 +2,34 @@
 
 """#LIBRERIAS"""
 
-#Importo librerias
+# Importo librerias
 import pandas as pd
-import numpy as np
-
-from sklearn import preprocessing
-from sklearn import tree
-from sklearn.metrics import confusion_matrix
-
-
-from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
+# import numpy as np
+# from sklearn import preprocessing
+# from sklearn import tree
+# from sklearn.metrics import confusion_matrix
+# from sklearn.metrics import accuracy_score
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import GridSearchCV
 from Src.modelo_entrenamiento import random_forest
 from Src.modelo_entrenamiento import decision_tree
 
-# print(random_forest)
-# !pip install; random_forest
-
 """#OBTENCIÓN DEL DATA SET"""
 
-#Lee el csv y lo convierte a un df de pandas
+# Lee el csv y lo convierte a un df de pandas
 path = "test.csv"
 dataset_crudo = pd.read_csv(path)
-dataset_crudo
+# dataset_crudo
 
 Datos_test_normalizado = dataset_crudo
 
 """#**MODELO: ARBOL DE DESICIÓN (DT)**"""
-#import os
-#ruta_archivo = os.path.abspath("modelo_entrenamiento.py")
-#print(ruta_archivo)
-
+# import os
+# ruta_archivo = os.path.abspath("modelo_entrenamiento.py")
+# print(ruta_archivo)
 # from "C:\\Users\\Vir\\Desktop\\SDIA\\2-CIENTIFICO DE DATOS\\PP2\\PP2_MODELO_DT\\PP2_MODELO" ; import random_forest
-
-#Predecimos con los datos del TEST
+# Predecimos con los datos del TEST
 random_forest_prediction = random_forest.predict(Datos_test_normalizado)
 decision_tree_prediction = decision_tree.predict(Datos_test_normalizado)
-print("print random_forest_prediction : ",random_forest_prediction)
+print("print random_forest_prediction : ", random_forest_prediction)
